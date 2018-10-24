f9ab87294e0e5fed048f7b845c84ee8298c6fa4c
Jamiel Rahi <jamiel.nr@gmail.com>
2018-10-23 23:45:15 -0400

    transferring code

--- controls.py
+++ controls.py
@@ -1,8 +1,52 @@
-import settings
 from time import sleep
-a = settings.a
+
+def amap(x,in_min,in_max,out_min,out_max):
+    return (x - in_min)*(out_max - out_min)/(in_max - in_min) + out_min
+
+XLIMIT = 180
+
+class Axis:
+    # Axis index (in pyjoy), with max and min values
+    def __init__(self, p, pmax, pmin, n, nmax, nmin, tol)
+        self.p = p
+        self.pmax = pmax - tol
+        self.pmin = pmin + tol
+        self.n = n
+        self.nmax = nmax - tol
+        self.nmin = nmin + tol
+
+class Remote:
+
+    def __init__(self, joystick, xaxis, yaxis):
+        self.xaxis = xaxix
+        self.yaxis = yaxis
+
+    def getx(self):
+        xpos = joystick.get_axis(self.xaxis.p)
+        xneg = joystick.get_axis(self.xaxis.p)
+        if xpos > self.xaxis.pmin:
+            return amap(xpos, self.xaxis.pmin, self.xaxis.pmax, 0, XLIMIT)
+        elif xneg > self.xaxis.nmin:
+            return (-1)*(amap(xneg, self.xaxis.nmin, self.xaxis.nmax, 0, XLIMIT))
+        else return 0
+
+    def gety(self):
+        ypos = joystick.get_axis(self.yaxis.p)
+        yneg = joystick.get_axis(self.yaxis.p)
+        if ypos > self.yaxis.pmin:
+            return amap(ypos, self.yaxis.pmin, self.yaxis.pmax, 0, 255)
+        elif yneg > self.yaxis.nmin:
+            return (-1)*(amap(yneg, self.yaxis.nmin, self.yaxis.nmax, 0, 255))
+        else return 0
+
+
+
+
+
+# For an Arduino potentiometer
 class Pot:
-    def __init__(self, x_pin, y_pin, tol):
+    def __init__(self, arduino, x_pin, y_pin, tol):
+        a = arduino
         self.__x_pin = x_pin
         self.__y_pin = y_pin
         self.tol = 50
@@ -11,6 +55,7 @@ class Pot:
         a.pinMode(self.__y_pin, a.INPUT)
     
     def x(self):
+        print(a.analogRead(self.__x_pin))
         return a.analogRead(self.__x_pin)
        
 
@@ -19,97 +64,93 @@ class Pot:
         
 
 class Motor:
-    def __init__(self, en, in1, in2, db):
+    def __init__(self, arduino, en, in1, in2, db):
         print("Motor instantiated")
+        a = arduino
         self.__en = en
         self.__in1 = in1
         self.__in2 = in2
         self.deadband = db
         self.pwm = 0
         self.isStopped = True
-        a.digitalWrite(self.__in1, a.LOW);
-        a.digitalWrite(self.__in2, a.HIGH); 
-        a.analogWrite(self.__en, 0);
+        a.digitalWrite(self.__in1, a.LOW)
+        a.digitalWrite(self.__in2, a.HIGH) 
+        a.analogWrite(self.__en, 0)
         sleep(0.01)    
-        a.pinMode(self.__en, a.OUTPUT);
-        a.pinMode(self.__in1, a.OUTPUT);
-        a.pinMode(self.__in2, a.OUTPUT); 
+        a.pinMode(self.__en, a.OUTPUT)
+        a.pinMode(self.__in1, a.OUTPUT)
+        a.pinMode(self.__in2, a.OUTPUT)
 
     def forward(self):
         print("fw")
-        a.digitalWrite(self.__in1, a.LOW);
-        a.digitalWrite(self.__in2, a.HIGH);    
+        a.digitalWrite(self.__in1, a.LOW)
+        a.digitalWrite(self.__in2, a.HIGH)    
   
 
     def reverse(self):
         print("bw")
-        a.digitalWrite(self.__in2, a.LOW);     
+        a.digitalWrite(self.__in1, a.HIGH)
+        a.digitalWrite(self.__in2, a.LOW)     
   
-
-    def stop(self):
-        setSpeed(0);
-  
-    def setSpeed(pwm):
+    def setSpeed(self, pwm):
         if pwm > self.deadband:
             self.pwm = pwm
             self.isStopped = False
         else :
             self.pwm = pwm
             self.isStopped = True
-            forward()
+            self.forward()
         a.analogWrite(self.__en, self.pwm)
 
+    def stop(self):
+        self.setSpeed(0)
 
+# Simple control system
 class Control:
-    def __init__(self, left, right, pot, kx):
-        self.__kx = kx
-        self.__left = left
-        self.__right = right
-        self.__pot = pot
+    def __init__(self, arduino, left, right, pot):
+        a = arduino
+        self._left = left
+        self._right = right
+        self._pot = pot
 
     def listen(self):
-        x = self.__pot.x()
-        y = self.__pot.y()
-        l = self.__left
-        r = self.__right
-        tol = self.__pot.tol
-        center = self.__pot.center
+        # returns pwm value
+        y = self._pot.gety()
+        x = self._pot.getx()
+
+        l = self._left
+        r = self._right
 
         # Y Axis
-        if y < (center - tol):
-            pwm_y = a.map(y, (center - tol), 0, 0, 255)
-            l.reverse()
-            r.reverse()
-            l.setSpeed(pwm_y)
-            r.setSpeed(pwm_y)
-
-        elif y > (center + tol):
-            pwm_y = a.map(y, (center + tol), 1023, 0, 255);
+        if y > 0 :
+            #pwm_y = amap(ypos, self._pot.yaxis.pmin + ytol, 0, 0, 255)
             l.forward()
             r.forward()
-            l.setSpeed(pwm_y)
-            r.setSpeed(pwm_y)
+            l.setSpeed(abs(y))
+            r.setSpeed(abs(y))
+
+        elif y < 0:
+            #pwm_y = amap(y, (center + tol), 1023, 0, 255);
+            l.reverse()
+            r.reverse()
+            l.setSpeed(abs(y))
+            r.setSpeed(abs(y))
         else:
             l.stop()
             r.stop()
+        
 
-        if x < (center - tol):
-            # left turn
-            turn(x, -1, 0)
+        if x < 0:
+            # turn left
+            self.turn(x, -1)
 
-        elif x > (center + tol):
-            # right turn
-            turn(x, 1, 1023)
+        elif x > 0:
+            # turn right
+            self.turn(x, 1)
 
-    def turn(amt, dir, n):
-        k = self.__kx
-        l = self.__left
-        r = self.__right
-        lpw = l.pwm
-        rpw = r.pwm
-        tol = self.__pot.tol
-        ctr = self.__pot.center
-        x = a.map(amt, (ctr + tol), n, 0, 180);
+    def turn(self, x, dir):
+        l = self._left
+        r = self._right
 
         if l.isStopped and r.isStopped:
             # zero radius turn
@@ -119,13 +160,13 @@ class Control:
             else:
                 l.forward()
                 r.reverse()
-            l.setSpeed(x)
-            r.setSpeed(x)
+            l.setSpeed(abs(x))
+            r.setSpeed(abs(x))
         else:
-            l.setSpeed(lpw + k*dir*x)
-            r.setSpeed(rpw - k*dir*x)
+            l.setSpeed(l.pwm + dir*x)
+            r.setSpeed(r.pwm - dir*x)
 
         if lpw < 0: l.setSpeed(0)
-        if lpw > 255: l.setSpeed(255)
+        if lpw > XLIMIT: l.setSpeed(XLIMIT)
         if rpw < 0: r.setSpeed(0)
-        if rpw > 255: r.setSpeed(255)
+        if rpw > XLIMIT: r.setSpeed(XLIMIT)
--- proto.py
+++ proto.py
@@ -1,6 +1,7 @@
 from nanpy import (ArduinoApi, SerialManager)
 from time import sleep
-
+from controls import Axis, Remote, Motor, Control
+import pygame
 
 # H-bridge inputs
 # Motor 1
@@ -14,7 +15,6 @@ IN4 = 3
 # Digital i/o
 BTN = 4
 LED = 9
-
 pressed = False;
 
 try:
@@ -23,156 +23,26 @@ try:
 except:
   print("Failed to connect to Arduino")
 
-POTX = 0
-POTY = 1
-
-def amap(x,in_min,in_max,out_min,out_max):
-    return (x - in_min)*(out_max - out_min)/(in_max - in_min) + out_min
-
-class Pot:
-    def __init__(self, x_pin, y_pin, tol):
-        self.__x_pin = x_pin
-        self.__y_pin = y_pin
-        self.tol = 50
-        self.center = 512
-        a.pinMode(self.__x_pin, a.INPUT)
-        a.pinMode(self.__y_pin, a.INPUT)
-    
-    def x(self):
-        print(a.analogRead(self.__x_pin))
-        return a.analogRead(self.__x_pin)
-       
-
-    def y(self):
-        return a.analogRead(self.__y_pin)
-        
-
-class Motor:
-    def __init__(self, en, in1, in2, db):
-        print("Motor instantiated")
-        self.__en = en
-        self.__in1 = in1
-        self.__in2 = in2
-        self.deadband = db
-        self.pwm = 0
-        self.isStopped = True
-        a.digitalWrite(self.__in1, a.LOW)
-        a.digitalWrite(self.__in2, a.HIGH) 
-        a.analogWrite(self.__en, 0)
-        sleep(0.01)    
-        a.pinMode(self.__en, a.OUTPUT)
-        a.pinMode(self.__in1, a.OUTPUT)
-        a.pinMode(self.__in2, a.OUTPUT)
-
-    def forward(self):
-        print("fw")
-        a.digitalWrite(self.__in1, a.LOW)
-        a.digitalWrite(self.__in2, a.HIGH)    
-  
-
-    def reverse(self):
-        print("bw")
-        a.digitalWrite(self.__in1, a.HIGH)
-        a.digitalWrite(self.__in2, a.LOW)     
-  
-    def setSpeed(self, pwm):
-        if pwm > self.deadband:
-            self.pwm = pwm
-            self.isStopped = False
-        else :
-            self.pwm = pwm
-            self.isStopped = True
-            self.forward()
-        a.analogWrite(self.__en, self.pwm)
-
-    def stop(self):
-        self.setSpeed(0)
-
-class Control:
-    def __init__(self, left, right, pot, kx):
-        self.__kx = kx
-        self.__left = left
-        self.__right = right
-        self.__pot = pot
-
-    def listen(self):
-        x = self.__pot.x()
-        print(x)
-        print("found x")
-        y = self.__pot.y()
-        print("found y")
-        l = self.__left
-        r = self.__right
-        tol = self.__pot.tol
-        center = self.__pot.center
-
-        # Y Axis
-        if y < (center - tol):
-            pwm_y = amap(y, (center - tol), 0, 0, 255)
-            l.reverse()
-            r.reverse()
-            l.setSpeed(pwm_y)
-            r.setSpeed(pwm_y)
-
-        elif y > (center + tol):
-            pwm_y = amap(y, (center + tol), 1023, 0, 255);
-            l.forward()
-            r.forward()
-            l.setSpeed(pwm_y)
-            r.setSpeed(pwm_y)
-        else:
-            l.stop()
-            r.stop()
-        print("end of y axis")
-
-        if x < (center - tol):
-            # left turn
-            self.turn(x, -1, 0)
-
-        elif x > (center + tol):
-            # right turn
-            self.turn(x, 1, 1023)
-
-    def turn(self, amt, dir, n):
-        k = self.__kx
-        l = self.__left
-        r = self.__right
-        lpw = l.pwm
-        rpw = r.pwm
-        tol = self.__pot.tol
-        ctr = self.__pot.center
-        x = amap(amt, (ctr + tol), n, 0, 180);
-
-        if l.isStopped and r.isStopped:
-            # zero radius turn
-            if dir < 0:
-                l.reverse()
-                r.forward()
-            else:
-                l.forward()
-                r.reverse()
-            l.setSpeed(x)
-            r.setSpeed(x)
-        else:
-            l.setSpeed(lpw + k*dir*x)
-            r.setSpeed(rpw - k*dir*x)
-
-        if lpw < 0: l.setSpeed(0)
-        if lpw > 255: l.setSpeed(255)
-        if rpw < 0: r.setSpeed(0)
-        if rpw > 255: r.setSpeed(255)
-
-
-
 # Setup
-right = Motor(ENA, IN1, IN2, 80)
-left = Motor(ENB, IN3, IN4, 70)
-pot = Pot(POTX, POTY, 20)
-ctl = Control(right, left, pot, 1)
+pygame.init()
+if pygame.joystick.get_count() > 0:
+    joystick = pygame.joystick.Joystick(0)
+    joystick.init()
+    print("Detected ", joystick.get_name())
+else:
+    raise ValueError("No joystick detected!")
+
+right = Motor(a, ENA, IN1, IN2, 80)
+left = Motor(a, ENB, IN3, IN4, 70)
+x = Axis(0, 1.000, 0.000, 0, -1.000, 0, 0.1)
+y = Axis(5, 1.000, -1.000, 2, 1.000, -1.000, 0.1)
+ds3 = Remote(joystick, x, y)
+ctl = Control(a, right, left, ds3)
 
 a.pinMode(BTN, a.INPUT)
 a.pinMode(LED, a.OUTPUT)
-# try:
+
+# Loop
 while True:
     print(a.analogRead(0))
     print('h')
@@ -187,9 +57,6 @@ while True:
     ctl.listen()
     print("listened")
     sleep(0.02)
-
-#except:
-#    print("Event loop failed.")
 
 
 
